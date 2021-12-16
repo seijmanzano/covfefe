4d7bf796997fdc10d2d5e35d6ea8cab005595e72
seijmanzano <96243608+seijmanzano@users.noreply.github.com>
2021-12-16 22:04:06 +0800

    ok

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function onClickMenu(){
+	document.getElementById("menu").classList.toggle("change");
+	document.getElementById("nav").classList.toggle("change");
+	
+	document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+function validateForm() {
+  var x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+
+function changeTo() {
+	document.body.innerHTML = "Suppose to have more products!";
+}
